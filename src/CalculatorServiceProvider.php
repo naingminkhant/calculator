51f62724d2fdb0e1d5e230662645cee33e457fa8
naingminkhant <naingminkhant.cm@gmail.com>
2017-05-24 17:26:20 +0630

    Init Packages

---
+++ src/CalculatorServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace NaingMinKhant\Calculator;
+
+use Illuminate\Support\ServiceProvider;
+
+class CalculatorServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        include __DIR__.'/routes.php';
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->make('NaingMinKhant\Calculator\Controllers\CalculatorController');
+    }
+}
